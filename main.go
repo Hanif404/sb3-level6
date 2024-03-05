45798de00d7bfbdafb6a7060406e7916f7132b25
Hanif Kharismadini <hanif.kharismadini.04@gmail.com>
2024-03-05 10:04:23 +0700

    update sb3 level6

--- main.go
+++ main.go
@@ -3,28 +3,54 @@ package main
 import "fmt"
 
 // isPrime checks if a number is prime.
-func isPrime(n int, p chan bool) {
-	go func() {
-		defer close(p)
-		if n <= 1 {
-			p <- false
+func isPrime(n int) bool {
+	c := make(chan int)
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		go calcPrime(n, i, c)
+		if <-c == 0 {
+			return false
 		}
-
-		for i := 2; i*i <= n; i++ {
-			if n%i == 0 {
-				p <- false
-			}
-		}
-		p <- true
-	}()
+	}
+	return true
 }
 
+func calcPrime(n int, i int, c chan<- int) {
+	a := n % i
+	c <- a
+}
+
+// generatePrimes generates prime numbers up to a given limit.
 func generatePrimes(limit int) []int {
 	var primes []int
 	for i := 2; i <= limit; i++ {
-		p := make(chan bool)
-		go isPrime(i, p)
-		if <-p {
+		if isPrime(i) {
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
+
+// isPrime checks if a number is prime.
+func isPrimeOld(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// generatePrimes generates prime numbers up to a given limit.
+func generatePrimesOld(limit int) []int {
+	var primes []int
+	for i := 2; i <= limit; i++ {
+		if isPrimeOld(i) {
 			primes = append(primes, i)
 		}
 	}
--- main_test.go
+++ main_test.go
@@ -8,29 +8,33 @@ import (
 func TestIsPrime(t *testing.T) {
 	var tests = []struct {
 		a    int
-		want bool
+		want int
 	}{
-		{-1, false},
-		{1, false},
-		{2, true},
-		{16, false},
-		{17, true},
+		{1, 0},
+		{2, 1},
+		{5, 3},
+		{10, 4},
 	}
 
 	for _, tt := range tests {
 		testname := fmt.Sprintf("%d", tt.a)
-		p := make(chan bool)
 		t.Run(testname, func(t *testing.T) {
-			go isPrime(tt.a, p)
-			if <-p != tt.want {
-				t.Errorf("got %v, want %v", <-p, tt.want)
+			primes := generatePrimes(tt.a)
+			if len(primes) != tt.want {
+				t.Errorf("got %v, want %v", len(primes), tt.want)
 			}
 		})
 	}
 }
 
-func BenchmarkGeneratePrimes(b *testing.B) {
+func BenchmarkGeneratePrimesNew(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		go generatePrimes(1000)
+		generatePrimes(100)
+	}
+}
+
+func BenchmarkGeneratePrimesOld(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		generatePrimesOld(100)
 	}
 }
